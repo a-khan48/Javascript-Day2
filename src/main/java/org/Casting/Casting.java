cd67d7e881aec5b50222f64c96cc77e677a722c3
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-05-09 22:27:12 -0400

    casting

---
+++ src/main/java/org/Casting/Casting.java
@@ -0,0 +1,14 @@
+package org.Casting;
+
+public class Casting {
+    public static void main(String[] args) {
+        double duble = 112.35;
+        System.out.println("Double: "+ duble);
+        int largeNumber = (int)duble;
+        System.out.println("Int: " + largeNumber);
+        String Number = "49";
+        System.out.println("The string value is: "  + Integer.valueOf(Number));
+        int sameNumber = 49;
+        System.out.println("The int value is: " + largeNumber);
+    }
+}
